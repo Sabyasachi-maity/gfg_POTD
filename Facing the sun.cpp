d2aed0a65f954d2db698270d16b446d235d36c92
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-09-20 23:41:17 +0530

    Facing the sun c++

---
+++ Facing the sun.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+    int countBuildings(vector<int> &height) {
+        // code here
+         int ans = 1, mx = height[0];
+        for(auto x:height){
+            if(x>mx){
+                mx=x;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
