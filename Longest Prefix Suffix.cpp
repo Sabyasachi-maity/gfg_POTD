a3fcc7a46f5204b61d543b76facfe8b8aa8e919c
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-09-22 23:32:39 +0530

    Longest Prefix Suffix c++

---
+++ Longest Prefix Suffix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int lps(string str) {
+        // Your code goes here
+          int i=1,j=0,n=str.length();
+        vector<int> storeLength(n,0);
+        while(i<n){
+            if(str[i]==str[j])storeLength[i++]=++j;
+            else{
+                if(j>0)j=storeLength[j-1];
+                else i++;
+            }
+        }
+        return storeLength[n-1];
+    }
+};
